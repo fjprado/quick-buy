d865ca02e173a2b5a25256b5862b9daaa1d4cdf0
Fernando José Prado <f.j.prado@hotmail.com>
2019-09-24 22:27:58 -0300

    Validações e modelo Usuario

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
   selector: "app-login",
@@ -6,5 +7,16 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
+  public usuario;
+  public usuarioAutenticado: boolean;
 
+  constructor(){
+    this.usuario = new Usuario();
+  }
+
+  login() {
+    if (this.usuario.email == "email.com.br" && this.usuario.senha == "abc") {
+      this.usuarioAutenticado = true;
+    }
+  }
 }

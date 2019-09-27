b3257665d05fd4e15b96b7dd62f0cafc29f480b3
Fernando José Prado <f.j.prado@hotmail.com>
2019-09-27 00:00:10 -0300

    Validação de rotas e autenticação de usuário

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { Router, RouterStateSnapshot} from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -8,15 +9,15 @@ import { Usuario } from "../../modelo/usuario";
 })
 export class LoginComponent {
   public usuario;
-  public usuarioAutenticado: boolean;
 
-  constructor(){
+  constructor(private router: Router) {
     this.usuario = new Usuario();
   }
 
   login() {
     if (this.usuario.email == "email.com.br" && this.usuario.senha == "abc") {
-      this.usuarioAutenticado = true;
+      localStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate(['/']);
     }
   }
 }
